3e695f2a1e3b4d4a3f8dec1ca1fdbaadc53227cb
Caitlin <cbaker24601@gmail.com>
2016-11-10 16:54:04 -0500

    added seed data

--- QuestLog/QuestLog/Models/Models.cs
+++ QuestLog/QuestLog/Models/Models.cs
@@ -37,5 +37,48 @@ namespace QuestLog.Models
     public class QuestContext : DbContext
     {
         public DbSet<Quest> Quests { get; set; }
+
+        public QuestContext()
+        {
+            Database.SetInitializer<QuestContext>(new MyInitializer());
+        }
+
+        public class MyInitializer : DropCreateDatabaseIfModelChanges<QuestContext>
+        {
+            public override void InitializeDatabase(QuestContext context)
+            {
+                base.InitializeDatabase(context);
+                if (!context.Quests.Any(x => x.Name == "Your First Quests!"))
+                {
+                    context.Quests.Add(new Models.Quest()
+                    {
+                        Name = "Your First Quests!",
+                        Level = 1,
+                        Alignment = Quest.AlignmentSide.Neutral,
+                        Reward = Quest.RewardType.Supplies,
+                        ExperiencePoints = 10
+                    });
+                    context.Quests.Add(new Models.Quest()
+                    {
+                        Name = "Kill Kobolds",
+                        Level = 3,
+                        Alignment = Quest.AlignmentSide.Good,
+                        Reward = Quest.RewardType.Gold,
+                        ExperiencePoints = 30
+                    });
+                    context.Quests.Add(new Models.Quest()
+                    {
+                        Name = "Kick NPC",
+                        Level = 2,
+                        Alignment = Quest.AlignmentSide.Evil,
+                        Reward = Quest.RewardType.Fame,
+                        ExperiencePoints = 20
+                    });
+                    context.SaveChanges();
+                }
+            }
+        }
     }
+
+
 }
